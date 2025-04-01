794c5538644c5bbf50d3a9c0c60f10038d6ef620
Declan0811 <declandvdunn@gmail.com>
2025-03-31 21:14:35 -0400

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,7 +69,7 @@ public final class Constants {
       public static final double kLevel1 = 0;
       public static final double kLevel2 = 2;
       public static final double kLevel3 = 2;
-      public static final double kLevel4 = 7.5;
+      public static final double kLevel4 = 6.75;
     }
 
     public static final class IntakeSetpoints {
